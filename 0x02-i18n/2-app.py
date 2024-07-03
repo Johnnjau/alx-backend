25bc4b109e2aaffa5ea7cce19d91b01a91badfb9
John <johnnjau202@gmail.com>
2024-07-03 14:12:57 +0300

    2-app.py

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,81 @@
+#!/usr/bin/en#!/usr/bin/env python3
+"""Get locale from request
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """set the default config
+    """
+
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get the local language from request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """return Hello, world
+    """
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run()v python3
+"""Get locale from request
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """set the default config
+    """
+
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get the local language from request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """return Hello, world
+    """
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
